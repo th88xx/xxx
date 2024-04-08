87380227e4d5bd1bda267ffb8272b3d60cd65607
Thiago H <156913280+th88xx@users.noreply.github.com>
2024-04-08 17:53:32 -0300

    Add files via upload

---
+++ projeto_C/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void main() {
+   int numero1;
+   int numero2;
+   int resultado;
+   numero1 = 10;
+   numero2 = 20;
+   resultado = numero1+numero2;
+   printf("Resultado: %d\n",resultado);
+   printf("Boa Tarde!\n");
+   printf("\n");
+   printf("Hello World");
+}
+
+
